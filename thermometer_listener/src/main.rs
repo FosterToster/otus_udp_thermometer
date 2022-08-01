74851f0db1530b6713a45e3aca5a695b6c0305b5
FosterToster <dimatsuv@gmail.com>
2022-08-02 00:05:23 +0700

    old code removed

--- thermometer_listener/src/main.rs
+++ thermometer_listener/src/main.rs
@@ -8,30 +8,13 @@ use thermometer_listener::ThermometerListener;
 async fn main() {
     if let Some(listener) = ThermometerListener::new().await {
         // shared variable for received temperature measurement
-        // let last_measure = Arc::new(Mutex::new(String::from("N/A")));
         let last_measure = Rc::new(RefCell::new(String::from("N/A")));
+
         tokio::join!(
             loop_receive(listener, last_measure.clone()),
             loop_display(last_measure)
         );
-        // starting a  with thermometer listener
-        // let last_measure_clone = last_measure.clone();
-        // let _ = std::thread::spawn(move || loop {
-        //     let new_measure = listener.receive_measure();
-        //     let mut lg = last_measure_clone.lock().unwrap();
 
-        //     *lg = new_measure;
-
-        //     std::mem::drop(lg);
-        // });
-
-        // displaying last known measurement
-        // loop {
-        //     let lg = last_measure.lock().unwrap();
-        //     println!("last known measure: {}", lg);
-        //     std::mem::drop(lg);
-        //     std::thread::sleep(std::time::Duration::from_secs(1))
-        // }
     } else {
         println!("Unable to create ThermometerListener");
     }
